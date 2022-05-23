ead1b5dfbb1c60aa855ebd6e03d5df1926c2f5ec
Bart Fiten <bart.fiten@asml.com>
2022-05-23 13:17:19 +0200

    bla

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,10 @@
 # Runner script for all modules
+from datetime import date
+from datetime import datetime
+
 from load_data import load_sensor_data
+from house_info import HouseInfo
+
 
 ##############################
 # Do not remove these two lines
@@ -15,8 +20,20 @@ print(f'loaded records: {len(data)}')
 
 # Module 2 code here:
 
+house_info = HouseInfo(data)
+test_area = 1
+recs = house_info.get_data_by_area('id', rec_area=test_area)
+
+print(f'\nHouse sensor records for area {test_area} = {len(recs)}')
+
+test_date = datetime.strptime('5/9/20', '%m/%d/%y')
+recs = house_info.get_data_by_date('id', rec_date=test_date)
+
+print(f'\nHouse sensor records for date {test_date.strftime("%m/%d/%y")} = {len(recs)}')
+
 # Module 3 code here:
 
 # Module 4 code here:
 
 # Module 5 code here:
+
